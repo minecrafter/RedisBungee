5a8328a700bb690dd13107302fd544c9419e3f2d
Tux <write@imaginarycode.com>
2014-12-07 16:13:10 -0500

    header

--- src/main/java/io/minimum/minecraft/redisbungee/players/GenericRedisBungeePlayer.java
+++ src/main/java/io/minimum/minecraft/redisbungee/players/GenericRedisBungeePlayer.java
@@ -1,10 +1,15 @@
+/**
+ * Copyright © 2013 tuxed <write@imaginarycode.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
 package io.minimum.minecraft.redisbungee.players;
 
 import io.minimum.minecraft.redisbungee.RedisBungee;
 import lombok.AllArgsConstructor;
 
 import java.net.InetAddress;
-import java.util.Calendar;
 import java.util.UUID;
 
 /**
